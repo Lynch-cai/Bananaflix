4dc54894561020a29496300bcc3d2168426e80b5
Lynchcai <41620147+Lynchcai@users.noreply.github.com>
2019-11-28 21:06:11 +0100

    Add icons & Add functionnal play pause button & volume controler

---
+++ scripts/video_player.js
@@ -0,0 +1,32 @@
+class Video_player{
+    constructor(){
+        this.$video_player = document.querySelector('.js_video_player')
+        this.$video = this.$video_player.querySelector('video')
+        this.$play_pause_button = this.$video_player.querySelector('.js_play_pause_button')
+        this.$play_pause_button_icon = this.$play_pause_button.querySelectorAll('svg')
+        this.set_play_pause()
+        this.set_volume()
+    }
+    // play pause button
+    set_play_pause(){
+        this.$play_pause_button.addEventListener(
+            'click', 
+            ()=>{
+                this.$play_pause_button_icon.forEach(($, key)=>{
+                    $.classList.toggle('fade-out')
+                    if (key == 0 && this.$play_pause_button_icon[key].classList.contains('fade-out'))
+                        this.$video.play()
+                    else if (key == 1 && this.$play_pause_button_icon[key].classList.contains('fade-out'))
+                        this.$video.pause()
+                })
+            }
+        )
+    }
+    set_volume(){
+        
+    }
+}
+
+
+
+const video_player = new Video_player()
